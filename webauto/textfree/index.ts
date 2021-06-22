0b131cdb41231322b0393b9c73a6fd6f04ba6dc5
lanly_dev <lanly-dev@users.noreply.github.com>
2021-06-21 18:52:59 -0700

    ml dir

--- webauto/textfree/index.ts
+++ webauto/textfree/index.ts
@@ -2,22 +2,23 @@ import { config } from 'dotenv'
 import { chromium } from 'playwright-chromium'
 config()
 const MSG = 'HELLOWOLRD'
+const { USER, PASS, PHONE } = process.env
 
 async function run() {
   const browser = await chromium.launch({
-    headless: true
+    headless: false
   })
   const constext = await browser.newContext()
   const page = await constext.newPage()
   page.goto('https://messages.textfree.us/login')
 
-  await page.fill('input[type="text"]', process.env.USER)
-  await page.fill('input[type="password"]', process.env.PASS)
+  await page.fill('input[type="text"]', USER!)
+  await page.fill('input[type="password"]', PASS!)
   await page.click('button[type="submit"]')
 
   await page.click('div[id="SyncContactsXDismissPopup"]')
   await page.click('div[id="startNewConversationButton"]')
-  await page.fill('#contactInput', process.env.PHONE)
+  await page.fill('#contactInput', PHONE!)
   await page.keyboard.press('Enter')
   await page.focus('.emojionearea-editor')
   await page.keyboard.type(MSG)
