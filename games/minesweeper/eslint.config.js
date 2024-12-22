5522985a0e6b376f1e3a9e82805bd1f83d37cd67
Lan Ly <lanly-dev@users.noreply.github.com>
2024-12-22 11:51:42 -0800

    setup

---
+++ games/minesweeper/eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    'comma-dangle': [`error`, `never`],
+    'no-throw-literal': `warn`,
+    'quote-props': [`error`, `as-needed`],
+    curly: [`error`, `multi-or-nest`],
+    eqeqeq: `error`,
+    quotes: [`error`, `backtick`],
+    semi: [`error`, `never`]
+  }
+}
